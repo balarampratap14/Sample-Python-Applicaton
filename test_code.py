ab4eb7b65ac2650113112329a93cccc2084e5d6f
Balaram <500061550@stu.upes.ac.in>
2020-03-11 04:08:37 +0530

    All Test Cases are Done by Tester1

--- test_code.py
+++ test_code.py
@@ -12,3 +12,19 @@ class TestHello(unittest.TestCase):
         rv = self.app.get('/')
         #self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World! Your Python Application Is succesfully Up and Running.\n Created and Written By Balaram Pratap')
+    def test_hello_hello(self):
+        rv = self.app.get('/hello/')
+        #self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World! Your Python Application Is succesfully Up and Running.\n Created and Written By Balaram Pratap')
+
+    def test_hello_name(self):
+        #name = 'Balaram_Pratap'
+        rv = self.app.get('/hello/Balaram_Pratap/')
+        #self.assertEqual(rv.status, '200 OK')
+        self.assertEqual('HEY! Hello Balaram_Pratap!\n. Welcome to my UserPage.!!!!!', 'HEY! Hello Balaram_Pratap!\n. Welcome to my UserPage.!!!!!')
+
+if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    unittest.main()
