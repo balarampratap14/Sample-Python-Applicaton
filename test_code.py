e68f452eaecce028d8d0ada04ea41ba4cbfa0cad
Balaram <500061550@stu.upes.ac.in>
2020-03-11 03:56:30 +0530

    Code for Main Hello World Page

--- test_code.py
+++ test_code.py
@@ -1,1 +1,8 @@
-# This is the file that need to be edited by any Testers Only "
+#!/usr/bin/env python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World! Your Python Application Is succesfully Up and Running.\n Created and Written By Balaram Pratap'
