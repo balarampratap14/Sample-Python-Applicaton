9aabaf8ad632249e4eb2042d1f3d273000907c46
Balaram <500061550@stu.upes.ac.in>
2020-03-11 03:59:37 +0530

    First Test Case

--- test_code.py
+++ test_code.py
@@ -1,1 +1,14 @@
-# This is the file that need to be edited by any Testers Only "
+#!/usr/bin/env python
+import unittest
+import application
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        #self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World! Your Python Application Is succesfully Up and Running.\n Created and Written By Balaram Pratap')
