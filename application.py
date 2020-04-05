44416ca14cd27a7be42a3abdad3ba867d5995783
BalaramPratap <35555297+balarampratap14@users.noreply.github.com>
2020-04-05 20:16:07 +0530

    Updating Changes according to internship-2 needs

--- application.py
+++ application.py
@@ -3,13 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello/')
-def hello_world():
-    return 'Hello World! Your Python Application Is succesfully Up and Running.\n Created and Written By Balaram Pratap'
+    def hello_world():
+        return 'Hello World!'
 
-@app.route('/hello/<username>') # dynamic route
-def hello_user(username):
-    return 'HEY! Hello %s!\n. Welcome to my UserPage.!!!!!' % username
+#@app.route('/hello/<username>') # dynamic route
+#def hello_user(username):
+   # return 'HEY! Hello %s!\n. Welcome to my UserPage.!!!!!' % username
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')     # open for everyone
